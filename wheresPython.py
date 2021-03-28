2d306af9c04af056b1a721fda2ba8a4a4f32dc65
Conoco75 <Conoco75@yahoo.com>
2021-03-28 17:31:56 -0500

    1st Staged Changes

--- wheresPython.py
+++ wheresPython.py
@@ -1,3 +1,4 @@
+#https://www.youtube.com/watch?v=-nh9rCzPJ20&t=1293s ~git 42 min mark
 import sys
 print("Python Path: {}".format(sys.executable))
 print ("Python Version: {}".format(sys.version))
@@ -7,6 +8,6 @@ def greet(who2greet):
     greeting = "Hello, {}".format(who2greet)
     return greeting
 
-print(greet("GIT"))
+print(greet("GIT_2"))
 print(greet("Steve"))
 #47:47 GIT`
